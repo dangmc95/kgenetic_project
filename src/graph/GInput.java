5c000973c0504a9ac8eec835d2d82f9b673aa8b2
tuanlekhac <ktuanlamson@gmail.com>
2016-03-12 18:23:11 +0700

    update
    update

--- src/graph/GInput.java
+++ src/graph/GInput.java
@@ -1,15 +1,48 @@
 package graph;
 
+import java.io.File;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+
+import utils.FileUtils;
 
 /**
  * @author ktuan
  */
 public class GInput {
 	private static Logger logger = Logger.getLogger(GInput.class);
-	private static final String FOLDER_DATA="data";
+	private static final String FOLDER_DATA = "data";
+	private static GInput INSTANCE = null;
+
+	private static synchronized GInput getInstance() {
+		if (INSTANCE != null)
+			INSTANCE = new GInput();
+		return INSTANCE;
+	}
+
+	private KGraph generateFromFile(File file) {
+		try{
+			List<String> lines= FileUtils.readMultilinesFromFile(file);
+			if (lines== null) return null;
+			return null;	
+		}catch(Exception ex){
+			logger.error(ex.getMessage());
+			return null;
+		}
+	}
+
 	public static void main(String[] args) {
 		logger.info("Start: GInput");
+		List<File> files = FileUtils.getFilesFromFolder(FOLDER_DATA);
+		for (File file : files) {
+			logger.info("\n" + file.getAbsolutePath());
+		}
+		logger.info("Size: " + files.size());
+		GInput input = GInput.getInstance();
+		for (File file : files) {
+			KGraph graph = input.generateFromFile(file);
+		}
 		logger.info("Finish: GInput");
 	}
 }
--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,15 +1,57 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 /**
  * @author ktuan
  */
 public class FileUtils {
-	private static final Logger logger= Logger.getLogger(FileUtils.class);
+	private static final Logger logger = Logger.getLogger(FileUtils.class);
+
+	public static List<File> getFilesFromFolder(String foldername) {
+		try {
+			logger.info("Start: getFilesFromFolder");
+			File folder = new File(foldername);
+			if (folder.isDirectory()) {
+				List<File> res = new ArrayList<File>();
+				for (File file : folder.listFiles()) {
+					if (file.isFile()) {
+						res.add(file);
+					}
+				}
+				return res;
+			}
+			logger.info("Finish: getFilesFromFolder");
+			return null;
+		} catch (Exception ex) {
+			logger.error(ex.getMessage());
+			return null;
+		}
+	}
+
+	public static List<String> readMultilinesFromFile(File file) {
+		try{
+			logger.info("Start: readMultilinesFromFile");
+			FileReader fr= new FileReader(file);
+			BufferedReader br= new BufferedReader(fr);
+			br.close();
+			fr.close();
+			logger.info("Finish: readMultilinesFromFile");
+			return null;
+		}catch(Exception ex){
+			logger.error(ex.getMessage());
+			return null;
+		}
+	}
+
 	public static void main(String[] args) {
 		logger.info("Start: FileUtils");
 		logger.info("Finish: FileUtils");
 	}
-
 }
